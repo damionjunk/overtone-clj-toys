2f4db33fe8c1c603a1b919593410daebf46b9afa
Damion Junk <djunk@indiana.edu>
2011-12-20 19:15:28 -0500

    first commit

---
+++ src/overtone_clj_toys/core.clj
@@ -0,0 +1,1 @@
+(ns overtone-clj-toys.core)
---
+++ test/overtone_clj_toys/test/core.clj
@@ -0,0 +1,6 @@
+(ns overtone-clj-toys.test.core
+  (:use [overtone-clj-toys.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
